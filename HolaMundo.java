8dc03cb4427ae4d4fa81660f18f75eb9e775063e
Javier Choque <unmonarca2010@gmail.com>
2011-05-15 00:13:28 -0300

    modifique una puteada por otr jaja.

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Al fin lpm!! ...");
+		System.out.println("que boquita ehh, quien te enseñó a decir eso, pendejo de mierda y la reputa que te parió..");
 	}
 }
