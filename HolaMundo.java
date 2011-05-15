811ed697925563cb798cbbafd271615adcf05cad
Javier Choque <unmonarca2010@gmail.com>
2011-05-15 00:34:55 -0300

    a ver si salta el lio..

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Chau ...");
+		System.out.println("podes eschedulear esto??? diria eugenio");
 	}
 }
